dca393dee7daf5b912edc438170f95594b242c09
Thomas-D40 <alexander.darracq@gmail.com>
2024-02-03 09:41:33 +0100

    feat: css + dl csv

--- src/pages/MainMenu.jsx
+++ src/pages/MainMenu.jsx
@@ -102,28 +102,60 @@ export default function Main() {
       return (
         <>
           {
-            <table>
+            <>
               {keys.map((key) => {
                 return (
-                  <>
-                    <tr>Phase {parseInt(key) + 1}</tr>
+                  <div className="phase-container">
+                    <h3 className="phase-announcer">
+                      Phase {parseInt(key) + 1}
+                    </h3>
                     {epreuvePhaseTree[key].map((element) => {
                       return (
                         <>
-                          <td>
-                            Epreuve {element.epreuve} : Equipe{" "}
+                          <p className="match-container">
+                            <b>Epreuve {element.epreuve}</b> : Equipe{" "}
                             {element.firstTeam} VS Equipe {element.secondTeam}
-                          </td>
+                          </p>
                         </>
                       );
                     })}
-                  </>
+                  </div>
                 );
               })}
-            </table>
+            </>
           }
         </>
       );
+    }
+  };
+
+  const generateCSVFile = () => {
+    if (epreuvePhaseTree) {
+      let keys = Object.keys(epreuvePhaseTree);
+      let csvContent = "data:text/csv;charset=utf-8, \n";
+      csvContent += "Epreuve;Equipe1;Equipe2; \n";
+      keys.map((key) => {
+        csvContent += "Phase " + (parseInt(key) + 1) + "\n";
+        epreuvePhaseTree[key].map((element) => {
+          csvContent +=
+            "Epreuve " +
+            element.epreuve +
+            ";" +
+            "Equipe " +
+            element.firstTeam +
+            ";" +
+            "Equipe " +
+            element.secondTeam +
+            ";\n";
+        });
+      });
+
+      let encodedUri = encodeURI(csvContent);
+      // Create a link to download it
+      var pom = document.createElement("a");
+      pom.href = encodedUri;
+      pom.setAttribute("download", "RecapTournoi.csv");
+      pom.click();
     }
   };
 
@@ -131,7 +163,7 @@ export default function Main() {
     <div>
       <div>
         <h1>Générateur de brackets</h1>
-        <p>Nombre d'équipes :</p>
+        <h2>Nombre d'équipes :</h2>
         <input
           onChange={(e) => {
             setNbrEquipe(e.target.value);
@@ -150,7 +182,8 @@ export default function Main() {
           defaultValue={nbrEpreuve}
         ></input>
       </div> */}
-      <p>Arbre de tournoi :</p>
+      <h2>Arbre de tournoi :</h2>
+      <button onClick={() => generateCSVFile()}>Télécharger en CSV</button>
       {computeTable()}
     </div>
   );
